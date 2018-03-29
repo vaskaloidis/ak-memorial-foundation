22e8e67a127a1fa8aed55ad7c39b11e6dd9f9add
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-03-29 02:24:24 -0400

    Added Totals. Verified feature removed from prod

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,4 +6,28 @@ class Purchase < ApplicationRecord
 
   accepts_nested_attributes_for :product
   accepts_nested_attributes_for :user
+
+  def self.golf_package_total_sales
+    total = 0.00
+    Purchase.where(product_category: 'golf_package').each do |p|
+      total = total + p.amount
+    end
+    return total
+  end
+
+  def self.donation_total_sales
+    total = 0.00
+    Purchase.where(product_category: 'donation').each do |p|
+      total = total + p.amount
+    end
+    return total
+  end
+
+  def self.sponsor_total_sales
+    total = 0.00
+    Purchase.where(product_category: 'sponsor').each do |p|
+      total = total + p.amount
+    end
+    return total
+  end
 end
