00aa2cd46451baa2b3bdcc2fb2a139cbe4cd828c
Vas Kaloidis <vas.kaloidis@gmail.com>
2018-05-14 18:31:06 -0400

    Removed test from admin notifications mailer, now that Dev is finished

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,7 +3,7 @@ class AdminMailer < ApplicationMailer
 def registration_notifications(customer)
   @customer = customer
   User.where(admin: true).each do |admin|
-    mail(to: admin.email, subject: 'TEST - Athan Kaloidis Memorial: ' + customer.full_name + ' Registered for The Tournament')
+    mail(to: admin.email, subject: 'Athan Kaloidis Memorial: ' + customer.full_name + ' Registered for The Tournament')
   end
 end
 
